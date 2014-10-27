324444e1e164f2333ac0ac28da6d48a7e5c1a648
Joe Burgess <joe@flatironschool.com>
2014-10-27 18:33:02 -0400

    Update IBOutlets Issue #1

--- Open-Me/FISViewController.m
+++ Open-Me/FISViewController.m
@@ -9,6 +9,23 @@
 #import "FISViewController.h"
 
 @interface FISViewController ()
+@property (strong, nonatomic) IBOutlet UIView *buttonsContainer;
+@property (strong, nonatomic) IBOutlet UIButton *oneButton;
+@property (strong, nonatomic) IBOutlet UIButton *twoButton;
+@property (strong, nonatomic) IBOutlet UIButton *threeButton;
+@property (strong, nonatomic) IBOutlet UIButton *fourButton;
+@property (strong, nonatomic) IBOutlet UIButton *fiveButton;
+@property (strong, nonatomic) IBOutlet UIButton *sixButton;
+@property (strong, nonatomic) IBOutlet UIButton *sevenButton;
+@property (strong, nonatomic) IBOutlet UIButton *eightButton;
+@property (strong, nonatomic) IBOutlet UIButton *nineButton;
+@property (strong, nonatomic) IBOutlet UIButton *zeroButton;
+@property (strong, nonatomic) IBOutlet UIButton *divideButton;
+@property (strong, nonatomic) IBOutlet UIButton *multiplyButton;
+@property (strong, nonatomic) IBOutlet UIButton *subtractionButton;
+@property (strong, nonatomic) IBOutlet UIButton *additionButton;
+@property (strong, nonatomic) IBOutlet UIButton *equalButton;
+@property (strong, nonatomic) IBOutlet UIButton *decimalButton;
 
 @end
 
